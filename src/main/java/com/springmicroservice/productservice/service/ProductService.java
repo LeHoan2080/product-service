8508caa945c83ba8353c1d793f3640af81de3903
Hoàn Lê <leviethoan2080@gmail.com>
2025-05-10 21:48:36 +0700

    first commit

---
+++ src/main/java/com/springmicroservice/productservice/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.springmicroservice.productservice.service;
+
+import com.springmicroservice.productservice.dto.ProductRequest;
+import com.springmicroservice.productservice.dto.ProductRespone;
+import com.springmicroservice.productservice.model.Product;
+import com.springmicroservice.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product created: {}", product);
+    }
+
+    public List<ProductRespone> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::getProductRespone).toList();
+    }
+
+    private ProductRespone getProductRespone(Product product) {
+        return ProductRespone.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
