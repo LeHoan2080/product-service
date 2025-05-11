cb6339a3eb6d4a1c453a334949660dd6dcbc6124
Hoàn Lê <leviethoan2080@gmail.com>
2025-05-11 15:00:31 +0700

    add transactional annotation

--- src/main/java/com/springmicroservice/productservice/service/ProductService.java
+++ src/main/java/com/springmicroservice/productservice/service/ProductService.java
@@ -7,6 +7,7 @@ import com.springmicroservice.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class ProductService {
         log.info("Product created: {}", product);
     }
 
+    @Transactional(readOnly = true)
     public List<ProductRespone> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return products.stream().map(this::getProductRespone).toList();
